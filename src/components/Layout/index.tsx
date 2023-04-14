1abcb0422be63da6670ed46935ed9481396ccdda
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2023-04-14 10:48:00 -0500

    Add: number of products in cart to cart icon

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,27 +3,36 @@ import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar';
 
 import classes from './layout.module.css';
+import { useGetProductsInCart } from '../../context/ProductsContext';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const productInCart = useGetProductsInCart();
+  const amoutOfProductsInCart = productInCart.reduce(
+    (amoutOfProductsAcc, productInCart) => amoutOfProductsAcc + productInCart.amount,
+    0
+  );
+
   return (
     <Fragment>
       <header className={classes.header}>
-        <div className={classes['image-container']}>
-          <img
-            src="https://m.media-amazon.com/images/G/01/gno/sprites/nav-sprite-global-1x-reorg3._CB634609711_.png"
-            alt="amazon icon"
-            width="350"
-            height="450"
-            className={classes.image}
-          />
-        </div>
+        <Link to="">
+          <div className={classes['image-container']}>
+            <img
+              src="https://m.media-amazon.com/images/G/01/gno/sprites/nav-sprite-global-1x-reorg3._CB634609711_.png"
+              alt="amazon icon"
+              width="350"
+              height="450"
+              className={classes.image}
+            />
+          </div>
+        </Link>
 
         <SearchBar />
 
         <nav>
           <ul>
             <li>
-              <Link to="cart">Cart</Link>
+              <Link to="cart">Cart: {amoutOfProductsInCart}</Link>
             </li>
           </ul>
         </nav>
