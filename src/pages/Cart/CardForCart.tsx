dd2f5a415e7bdc23a333046a5a0d4482da3cdb0b
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2023-04-14 08:59:14 -0500

    Add: logic to remove products from the cart

--- src/pages/Cart/CardForCart.tsx
+++ src/pages/Cart/CardForCart.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { ProductType } from '../../types/ProductType';
 import { useSetProductsInCart } from '../../context/ProductsContext';
 
@@ -12,6 +12,12 @@ const CardForCart = ({ product }: { product: ProductType }) => {
       prevState.map((element) =>
         element.id === productId ? { ...product, amount: +event.target.value } : element
       )
+    );
+  };
+
+  const handleRemoveProductFroCard = (productId: number) => {
+    setProductsInCart((prevState) =>
+      prevState.filter((productInCart) => productInCart.id !== productId)
     );
   };
 
@@ -37,22 +43,28 @@ const CardForCart = ({ product }: { product: ProductType }) => {
           </Row>
 
           <Row>
-            <Form.Select
-              aria-label="Default select example"
-              size="sm"
-              defaultValue={amount}
-              onChange={handleChangeAmoutOfProductsInCart}>
-              <option value="1">cant: 1</option>
-              <option value="2">cant: 2</option>
-              <option value="3">cant: 3</option>
-              <option value="4">cant: 4</option>
-              <option value="5">cant: 5</option>
-              <option value="6">cant: 6</option>
-              <option value="7">cant: 7</option>
-              <option value="8">cant: 8</option>
-              <option value="9">cant: 9</option>
-              <option value="10">cant: 10</option>
-            </Form.Select>
+            <Col sm={2}>
+              <Form.Select
+                aria-label="Default select example"
+                size="sm"
+                defaultValue={amount}
+                onChange={handleChangeAmoutOfProductsInCart}>
+                <option value="1">cant: 1</option>
+                <option value="2">cant: 2</option>
+                <option value="3">cant: 3</option>
+                <option value="4">cant: 4</option>
+                <option value="5">cant: 5</option>
+                <option value="6">cant: 6</option>
+                <option value="7">cant: 7</option>
+                <option value="8">cant: 8</option>
+                <option value="9">cant: 9</option>
+                <option value="10">cant: 10</option>
+              </Form.Select>
+            </Col>
+
+            <Col>
+              <Button onClick={() => handleRemoveProductFroCard(productId)}>Eliminar</Button>
+            </Col>
           </Row>
         </Col>
       </Row>
