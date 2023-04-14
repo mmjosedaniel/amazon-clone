4c45467795c0f9177754e569916c1b6bd1daf724
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2023-04-13 21:00:58 -0500

    Add logic to manage amount of products in cart

---
+++ src/pages/Cart/CardForCart.tsx
@@ -0,0 +1,63 @@
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import { ProductType } from '../../types/ProductType';
+import { useSetProductsInCart } from '../../context/ProductsContext';
+
+const CardForCart = ({ product }: { product: ProductType }) => {
+  const { name: productName, price, imageURL, amount, id: productId } = product;
+
+  const setProductsInCart = useSetProductsInCart();
+
+  const handleChangeAmoutOfProductsInCart = (event: { target: { value: string } }) => {
+    setProductsInCart((prevState) =>
+      prevState.map((element) =>
+        element.id === productId ? { ...product, amount: +event.target.value } : element
+      )
+    );
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col sm={3}>
+          <img src={imageURL} width="150" />
+        </Col>
+
+        <Col>
+          <Row>
+            <Col>
+              <p>{productName}</p>
+            </Col>
+
+            <Col sm={2}>
+              <p>
+                US$
+                {` ${price}`}
+              </p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Form.Select
+              aria-label="Default select example"
+              size="sm"
+              defaultValue={amount}
+              onChange={handleChangeAmoutOfProductsInCart}>
+              <option value="1">cant: 1</option>
+              <option value="2">cant: 2</option>
+              <option value="3">cant: 3</option>
+              <option value="4">cant: 4</option>
+              <option value="5">cant: 5</option>
+              <option value="6">cant: 6</option>
+              <option value="7">cant: 7</option>
+              <option value="8">cant: 8</option>
+              <option value="9">cant: 9</option>
+              <option value="10">cant: 10</option>
+            </Form.Select>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CardForCart;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { useGetProductsInCart } from '../../context/ProductsContext';
+import CardForCart from './CardForCart';
+
 const Cart = (): React.ReactElement => {
-  return <h2>This is my cart component</h2>;
+  const productsInCart = useGetProductsInCart();
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          {productsInCart.map((product) => (
+            <CardForCart key={`product-card-in-Cart-${product.id}`} product={product} />
+          ))}
+        </Col>
+
+        <Col sm={3} style={{ background: 'yellow' }}>
+          <a>Total</a>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Cart;
